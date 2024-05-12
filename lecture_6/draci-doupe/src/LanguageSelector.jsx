e90344322d8dc00688544d92438f0e3d2d1f17bb
Ondra Kučera <ondrej.kucera@centrum.cz>
2024-05-12 20:29:42 +0200

    Lecture 6: add draci-doupe

---
+++ lecture_6/draci-doupe/src/LanguageSelector.jsx
@@ -0,0 +1,24 @@
+import { useLanguageContext } from "./LanguageContext";
+
+const LANGUAGES = [
+	{ code: "cs", name: "Čeština" },
+	{ code: "en", name: "English" },
+];
+
+export const LanguageSelector = () => {
+	const { language, setLanguage } = useLanguageContext();
+
+	const handleLanguageChange = (event) => {
+		setLanguage(event.target.value);
+	};
+
+	return (
+		<select value={language} onChange={handleLanguageChange}>
+			{LANGUAGES.map((cbLanguage) => (
+				<option key={cbLanguage.code} value={cbLanguage.code}>
+					{cbLanguage.name}
+				</option>
+			))}
+		</select>
+	);
+};
